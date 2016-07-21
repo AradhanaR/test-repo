4d938d6d3d2a3d97db3cb3c1e158c072666c61a7
aradhana-geotech <aradhana.rani@geotechinfo.net>
2016-07-21 13:53:57 +0530

    fix

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -33,8 +33,8 @@ class AppKernel extends Kernel
         // when the application runs in 'dev' or 'test' environments. This allows
         // to increase application performance in the production environment.
         if (in_array($this->getEnvironment(), ['dev', 'test'])) {
-            //$bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
-            //$bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+            $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
+            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             //$bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             //$bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
         }
